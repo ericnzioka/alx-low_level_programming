8305f1284a0dbf8612059e2bd26be1a9ff417c2c
Eric Muia <erick.muia.n@gmail>
2023-02-16 04:26:36 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 	printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
-	printf("Size of float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }

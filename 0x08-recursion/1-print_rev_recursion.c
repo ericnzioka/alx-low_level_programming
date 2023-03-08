f15d6cab994ac8fb6bc5b302490dfb56c632a321
Eric Muia <erick.muia.n@gmail>
2023-03-08 03:18:03 -0800

    check 2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,20 +1,15 @@
 #include "main.h"
-#include <string.h>
 /**
  *_print_rev_recursion- print a string in a new line
  *@s: string to be reversed
+ *
  */
-
-int len = strlen(*s);
-
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar(*(s + len));
-		_print_rev_recursion(s);
-		len --;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	else 
-		_putchar('\n')
+
 }

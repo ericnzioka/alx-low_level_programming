6d41dee7847872a2992615d9ff51b6098e885c9f
Eric Muia <erick.muia.n@gmail>
2023-03-20 13:09:38 -0700

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,19 +1,13 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
- * @print_dog: prints 'struct dog
- * * @dog: struct
- * @d: pointer
+ *print_dog- print struct dog
+ *@d: the dog to be intialized
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-	{
-		printf("");
-	}
-	else
+	if (d != NULL)
 	{
 		if (d->name != NULL)
 			printf("Name: %s\n", d->name);

71ad2089e74fb9b9ae65bc87ff76e9842abf6089
Eric Muia <erick.muia.n@gmail>
2023-03-24 08:02:15 -0700

    m

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,12 +12,14 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list parameters;
-	int i, sum = 0;
-
+	unsigned int i, sum = 0;
+	
+	if (n == 0)
+		return (0);
 	va_start(parameters,n);
 	
 	for (i = 0; i < n; i++)
-		sum += va_arg(parameters, int);
+		sum = sum + va_arg(parameters, int);
 	va_end(parameters);
 	return (sum);
 }

76bac626462e5ebf7540eb549461d53ce7a8abfc
Eric Muia <erick.muia.n@gmail>
2023-04-11 09:40:45 -0700

    check it again

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,14 +6,16 @@
  *result: converted int
  *Return: converted number or if b is NULL or contains chars not 0 or 1 - 0.
  */
+
 unsigned int binary_to_uint(const char *b) {
     unsigned int result = 0;
+    int len;
 
     if (b == NULL) {
         return 0;
     }
 
-    int len = strlen(b);
+    len = strlen(b);
     for (int i = 0; i < len; i++) {
         if (b[i] == '0') {
             result = (result << 1) | 0;
